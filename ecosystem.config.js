511205493b39b08061f8580a758e41a6f0f736ae
himanthajaga <himanthagamachchige@gmail.com>
2026-03-16 09:44:33 +0530

    Update service configuration to use dynamic port and add Eureka instance ID

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps : [{
+    name   : "Enrollment-Service",
+    script : "java -jar ./Enrollment-Service/target/Enrollment-Service-1.0.0.jar",
+    log_file: "./logs/enrollment-service-out.log",
+    instances: 2,
+  },
+    {
+      name   : "program-Service",
+      script : "java -jar ./program-Service/target/Program-Service-1.0.0.jar",
+      log_file: "./logs/program-service-out.log",
+      instances: 2,
+    },
+    {
+      name   : "Student-Service",
+      script : "java -jar ./Student-Service/target/Student-Service-1.0.0.jar",
+      log_file: "./logs/student-service-out.log",
+      instances: 2,
+    }
+  ],
+}
