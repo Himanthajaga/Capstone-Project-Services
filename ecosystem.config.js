f10e5c1c76cabd5890f9579fca0d7e0953c2a765
himanthajaga <himanthagamachchige@gmail.com>
2026-03-17 15:47:26 +0530

    Integrate Google Cloud Storage for image handling and update configuration files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -21,7 +21,10 @@ module.exports = {
       name   : "Student-Service",
       script : "java -jar ./Student-Service/target/Student-Service-1.0.0.jar",
       log_file: "./logs/student-service-out.log",
-      instances: 2
+      instances: 2,
+      env: {
+        GOOGLE_APPLICATION_CREDENTIALS: "/opt/keys/student-service-key.json"
+      }
     }
   ],
 }
