2454da1bb3b549ae4d2c306e0db41e3923bc41e4
himanthajaga <himanthagamachchige@gmail.com>
2026-03-22 10:39:55 +0530

    chore: update cloud-sql-auth-proxy script to use mysql-vm2 instance

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [
     {
         name   : "cloud-sql-auth-proxy",
-        script : "./cloud-sql-proxy capstone-project-490416:asia-southeast1:mysql-vm capstone-project-490416:asia-southeast1:postgres-vm --private-ip",
+        script : "./cloud-sql-proxy capstone-project-490416:asia-southeast1:mysql-vm2 capstone-project-490416:asia-southeast1:postgres-vm --private-ip",
         log_file: "./logs/cloud-sql-auth-proxy-out.log",
     },
       {
